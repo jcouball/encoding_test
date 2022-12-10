8469edef98f8a512ee1f6e4239445274ca67bf34
James Couball <jcouball@yahoo.com>
2022-12-10 15:32:40 -0800

    first commit

---
+++ encoding_test.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+puts
+puts "Encoding.default_external: #{Encoding.default_external}"
+puts
+puts "Encoding.default_internal: #{Encoding.default_internal}"
+puts
+puts "Encoding.list: #{Encoding.list}"
+puts
+puts "Encoding.name_list: #{Encoding.name_list}"
+puts
+puts "Encoding.aliases: #{Encoding.aliases}"
+puts
